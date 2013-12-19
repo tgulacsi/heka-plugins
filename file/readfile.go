0ce19a5438a11c20cdb5f829500379e015f96ed3
Tamás Gulácsi <tgulacsi78@gmail.com>
2013-12-19 18:50:54 +0100

    ReadFileFilter: add GetFile method to Message

--- file/readfile.go
+++ file/readfile.go
@@ -24,87 +24,107 @@ import (
 	"github.com/mozilla-services/heka/pipeline"
 )
 
-// FileReader plugin for expanding messages from external files based on template
+// FileReadFilter plugin for expanding messages from external files based on template
 // in the template you can use the Message's fields
-type FileReader struct {
+type FileReadFilter struct {
 	tmpl    *template.Template
 	newType string
 }
 
 // Extract hosts value from config and store it on the plugin instance.
-func (fr FileReader) Init(config interface{}) (err error) {
+func (fr *FileReadFilter) Init(config interface{}) (err error) {
 	var (
 		value interface{}
 		str   string
 		ok    bool
 	)
 	conf := config.(pipeline.PluginConfig)
-	if value, ok = conf["pattern"]; !ok {
-		return errors.New("FileReader: No 'pattern' setting specified.")
-	}
-	if str, ok = value.(string); !ok {
-		return errors.New("FileReader: 'pattern' setting not a string value.")
-	}
-	fr.tmpl, err = template.New("filereader").Parse(str)
-
 	if value, ok = conf["newType"]; !ok {
-		return errors.New("FileReader: No 'newType' setting specified.")
+		return errors.New("FileReadFilter: No 'newType' setting specified.")
 	}
 	if fr.newType, ok = value.(string); !ok {
-		return errors.New("FileReader: 'newType' setting not a string value.")
+		return errors.New("FileReadFilter: 'newType' setting not a string value.")
 	}
 
+	if value, ok = conf["pattern"]; !ok {
+		return errors.New("FileReadFilter: No 'pattern' setting specified.")
+	}
+	if str, ok = value.(string); !ok {
+		return errors.New("FileReadFilter: 'pattern' setting not a string value.")
+	}
+
+	if fr.tmpl, err = template.New("filereader").Parse(str); err == nil && fr.tmpl == nil {
+		return fmt.Errorf("FileReadFilter: empty template (%q)", str)
+	}
 	return err
 }
 
-// Run runs the FileReader filter, which inspects each message, and appends
+type extendedMessage struct {
+	*message.Message
+}
+
+func (m extendedMessage) GetField(name string) string {
+	f := m.Message.FindFirstField(name)
+	if f != nil {
+		return f.ValueString[0]
+	}
+	return ""
+}
+
+// Run runs the FileReadFilter filter, which inspects each message, and appends
 // the content of the file named as the executed template to the existing payload.
 // The resulting message will be injected back, and have newType type.
-func (fr FileReader) Run(r pipeline.FilterRunner, h pipeline.PluginHelper) (err error) {
+func (fr FileReadFilter) Run(r pipeline.FilterRunner, h pipeline.PluginHelper) (err error) {
+	if fr.tmpl == nil {
+		return errors.New("FileReadFilter: empty template")
+	}
 	var (
 		fh           *os.File
 		npack, opack *pipeline.PipelinePack
 	)
 	out := bytes.NewBuffer(make([]byte, 0, 4096))
+	log.Printf("FileReadFilter: Starting with template %s", fr.tmpl)
 	for opack = range r.InChan() {
-		if opack.Decoded {
-			out.Reset()
-			if err = fr.tmpl.Execute(out, opack.Message); err != nil {
-				opack.Recycle()
-				return fmt.Errorf("FileReader: error executing template %v with message %v: %v",
-					fr.tmpl, opack.Message, err)
-			}
-			if fh, err = os.Open(out.String()); err != nil {
-				opack.Recycle()
-				return fmt.Errorf("FileReader: cannot read %q: %v", out, err)
-			}
-			out.Reset()
-			//if _, err = io.Copy(out, io.LimitedReader{R: fh, N: 65000}); err != nil && err != io.EOF {
-			if _, err = io.Copy(out, fh); err != nil && err != io.EOF {
-				opack.Recycle()
-				fh.Close()
-				return fmt.Errorf("FileReader: error reading %q: %v", fh.Name(), err)
-			}
-			fh.Close()
-
-			npack = h.PipelinePack(opack.MsgLoopCount)
-			if npack == nil {
-				opack.Recycle()
-				return errors.New("FileReader: no output pack - infinite loop?")
-			}
-			npack.Decoded = true
-			npack.Message = message.CopyMessage(opack.Message)
-			npack.Message.SetType(fr.newType)
-			npack.Message.SetPayload(npack.Message.GetPayload() + "\n" + out.String())
-			if !r.Inject(npack) {
-				log.Printf("FileReader: cannot inject new pack %v", npack)
-			}
+		//log.Printf("opack=%v", opack)
+		//if opack.Decoded {
+		out.Reset()
+		if err = fr.tmpl.Execute(out, extendedMessage{opack.Message}); err != nil {
+			opack.Recycle()
+			return fmt.Errorf("FileReadFilter: error executing template %v with message %v: %v",
+				fr.tmpl, opack.Message, err)
 		}
+		log.Printf("out=%q", out)
+		if fh, err = os.Open(out.String()); err != nil {
+			opack.Recycle()
+			return fmt.Errorf("FileReadFilter: cannot read %q: %v", out, err)
+		}
+		out.Reset()
+		//if _, err = io.Copy(out, io.LimitedReader{R: fh, N: 65000}); err != nil && err != io.EOF {
+		if _, err = io.Copy(out, fh); err != nil && err != io.EOF {
+			opack.Recycle()
+			fh.Close()
+			return fmt.Errorf("FileReadFilter: error reading %q: %v", fh.Name(), err)
+		}
+		fh.Close()
+
+		npack = h.PipelinePack(opack.MsgLoopCount)
+		if npack == nil {
+			opack.Recycle()
+			return errors.New("FileReadFilter: no output pack - infinite loop?")
+		}
+		npack.Decoded = true
+		npack.Message = message.CopyMessage(opack.Message)
+		npack.Message.SetType(fr.newType)
+		npack.Message.SetPayload(npack.Message.GetPayload() + "\n" + out.String())
+		if !r.Inject(npack) {
+			log.Printf("FileReadFilter: cannot inject new pack %v", npack)
+		}
+		//}
 		opack.Recycle()
 	}
 	return nil
 }
 
 func init() {
-	pipeline.RegisterPlugin("FileReaderFilter", func() interface{} { return new(FileReader) })
+	pipeline.RegisterPlugin("FileReadFilter", func() interface{} { return new(FileReadFilter) })
 }
